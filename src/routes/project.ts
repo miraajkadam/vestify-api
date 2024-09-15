e22ba76ec43e37a990fe7f618893e699cb50a120
Zakiuddin53 <zakiuddin810@gmail.com>
2024-09-15 16:28:45 +0530

    Integrated Swagger UI

--- src/routes/project.ts
+++ src/routes/project.ts
@@ -4,10 +4,62 @@ import { addNewProject, deleteProject, getAllProjects } from '@/controllers/proj
 
 const projectRouter = Router()
 
+/**
+ * @swagger
+ * /api/project/new:
+ *   post:
+ *     summary: Add a new project
+ *     tags: [Projects]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/AddProjectApiPayload'
+ *     responses:
+ *       200:
+ *         description: Project added successfully
+ *       500:
+ *         description: Server error
+ */
 projectRouter.post('/new', addNewProject)
 
+/**
+ * @swagger
+ * /api/project/delete:
+ *   post:
+ *     summary: Delete a project
+ *     tags: [Projects]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/DeleteProjectApiPayload'
+ *     responses:
+ *       200:
+ *         description: Project deleted successfully
+ *       500:
+ *         description: Server error
+ */
 projectRouter.post('/delete', deleteProject)
 
+/**
+ * @swagger
+ * /api/project/getAll:
+ *   get:
+ *     summary: Get all projects
+ *     tags: [Projects]
+ *     responses:
+ *       200:
+ *         description: List of projects
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ProjectListResponse'
+ *       500:
+ *         description: Server error
+ */
 projectRouter.get('/getAll', getAllProjects)
 
 export default projectRouter
