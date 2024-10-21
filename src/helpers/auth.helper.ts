df24efa2458645522fc60fef308116cc0c5910f9
Miraaj Kadam <miraajkadam@gmail.com>
2024-10-21 22:56:21 +0530

    store cryptic password in db on user sign-up

--- src/helpers/auth.helper.ts
+++ src/helpers/auth.helper.ts
@@ -1,5 +1,8 @@
 import { AccountType } from '@prisma/client'
+import { genSalt, hash } from 'bcrypt'
 import { isEmail, isStrongPassword } from 'validator'
+
+const SALT_ROUNDS = 10
 
 /**
  * Validates the signup payload for a user.
@@ -23,7 +26,7 @@ export const validateSignupPayload = (
   email: string,
   password: string,
   accountType: AccountType
-) => {
+): boolean => {
   if (!username || typeof username !== 'string') return false
 
   if (!email || typeof email !== 'string' || !isEmail(email)) return false
@@ -33,4 +36,26 @@ export const validateSignupPayload = (
   if (accountType !== AccountType.VC && accountType !== AccountType.USER) return false
 
   return true
+}
+
+/**
+ * Generates a cryptographic hash of the given password using a generated salt.
+ *
+ * @async
+ *
+ * @param {string} password - The plaintext password to be hashed.
+ *
+ * @returns {Promise<string>} - A promise that resolves to the hashed password.
+ *
+ * @throws {Error} - Throws an error if hashing fails.
+ *
+ * @example
+ * const hashedPassword = await getCrypticPassword('mySecurePassword');
+ * console.log(hashedPassword); // Outputs the hashed password
+ */
+export const getCrypticPassword = async (password: string): Promise<string> => {
+  const salt = await genSalt(SALT_ROUNDS)
+  const hashedPassword = await hash(password, salt)
+
+  return hashedPassword
 }
